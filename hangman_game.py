1c9140e0e5f58adb58b246f148802c92c75098eb
Carlos Francisco Orejuela Lluncor <carlos_francisco_@hotmail.com>
2022-05-26 12:51:47 -0500

    Add database

--- hangman_game.py
+++ hangman_game.py
@@ -1,30 +1,65 @@
 #List and dicts comprehension
 #Manejo de errores y de archivos
-def run():
-     
-    dic = {}
-    dic_characters = {}
-    with open('./archivos/data.txt', 'r', encoding="utf-8") as f:
-        words_list = [i.strip('\n') for i in f]
-    for i, word in enumerate(words_list, 1):
-        dic[i] = word
-    for word in dic.values():
-        list_characters = []
-        for i in range (len(word)):
-            list_characters.append(word[i])
-            dic_characters[word] = list_characters
-    print(dic_characters)
-
-    """
-    word = 'sexo'
-    lista = []
+def slices(word):
+    list_characters = []
     for i in range(len(word)):
-        lista.append(word[i])
-    print(lista)
-    """ 
-    
+        list_characters.append(word[i])
+    return list_characters
+
+def database(word_list):
+    dic = {}
+    for word in word_list:
+        dic[word] = slices(word)
+    print(dic)
+
+def run():
+    with open('./archivos/data.txt', 'r', encoding="utf-8") as f:
+        words_list = [i.strip('\n') for i in f]   
+    database(words_list)
+
 if __name__=='__main__':
     run()
+
+"""
+import os
+import random
+
+def run():
+    lista_random = ['car', 'aire']
+    dic = {
+        'car': ['c', 'a', 'r'],
+        'aire': ['a','i','r','e']
+    }
+    lista_2 = []
+    palabra_aleatoria = random.choice(range(0, 2))
+    print(palabra_aleatoria)
+    for i, word in enumerate(lista_random):
+        if i == palabra_aleatoria:
+            llave = word
+            print(llave)
+    for k, v in dic.items():
+        if llave == k:
+            lista = v
+            for i in range(len(lista)):
+                lista_2.append('-')
+            print(''.join(lista_2))
+            for i in range(4):
+                attempt = input('')
+                for i, c in enumerate(lista):
+                    if c == attempt:
+                        lista_2[i] = attempt
+                os.system('cls')
+                print(''.join(lista_2))
+            if ((''.join(lista_2))== (''.join(lista))):
+                print('Ganaste')
+                break
+            else:
+                print('Malardo')
+
+if __name__=='__main__':
+    run()
+"""
+
 
 """
 #FUNCION ENUMERATE
@@ -39,5 +74,5 @@ lista = ['C++', 'JavaScript', 'Python']
         4: 'Sexo'
     }
     print(dic.get(5, "The key doesn't exist")) => 'The key doesn't exist'| Because 5 isn't in the dict
-    """
-    
+"""
+
